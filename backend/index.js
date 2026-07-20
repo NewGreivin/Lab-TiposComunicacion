08a9bb329a1e4ac9dcfaa86112b358cc3890effe
oscar <osalvarezcr@est.utn.ac.cr>
2026-07-20 10:11:52 -0600

    feat: Implementacion de Web Socket Back
    -Se descargo la dependencia nesesaria para que funcione el servidor ws
    -Nuevo archivo common wsManager que nos permite administrar todo la comunicación y configuración necesaria de ws
    -modificación de index.js importante el método para levantar el servidor ws y su respectiva dependencia, también se crea server http y se inicializa con el método importado, luego cambios la escucha del servidor por el server inicializado

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
+import http from "http";
 
 import app from "./app.js";
+import { inicializarWebSocket } from "./common/wsManager.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+inicializarWebSocket(server);
+
+server.listen(PORT, () => {
     console.log(`Servidor ejecutándose en puerto http://localhost:${PORT}`);
+    console.log(`Websocket chat diponible en ws://localhost:${PORT}/ws/mensajes`);
 });
